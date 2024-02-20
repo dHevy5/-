51a3de27a875b5a99615154b06193023bf7224ae
dHevy5 <160593982+dHevy5@users.noreply.github.com>
2024-02-20 14:23:35 +0400

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	int a;
+	cout << "Решите пример и напишите ответ: " << endl;
+	cout << "2+8-5= ";
+	cin >> a;
+
+	switch (a) {
+	case 5:
+	{
+		cout << "Ваш ответ верный!";
+		break;
+	}
+	default:
+	 {
+		cout << "Вы решили пример неправильно";
+	 	break;
+	 }
+	}
+	
+
+return 0;
+}
